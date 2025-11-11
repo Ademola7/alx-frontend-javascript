dd1282c09a40a83f524fbcc9686a66d3861c69f0
Ademola <demisterling007@gmail.com>
2025-11-11 08:11:07 +0100

    Fixing errors

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,8 +15,14 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-function printTeacher(firstName: string, lastName: string): string {
-  return `${firstName.charAt(0)}. ${lastName}`;
+function printTeacher({
+  firstName,
+  lastName,
+}: {
+  firstName: string;
+  lastName: string;
+}): string {
+  return `${firstName[0]}. ${lastName}`;
 }
 
 interface StudentConstructor {
