0c0efdcf748ef1bad18f16999a8fcc98d08bc242
Ademola <demisterling007@gmail.com>
2025-11-11 08:07:30 +0100

    Fixing errors

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,7 +7,7 @@ interface Teacher {
   [propName: string]: any;
 }
 
-interface Directors extends Teacher {
+interface Director extends Teacher {
   numberOfReports: number;
 }
 
@@ -38,7 +38,7 @@ class StudentClass implements StudentClassInterface {
   }
 
   workOnHomework(): string {
-    return 'Currently working';
+    return "Currently working";
   }
 
   displayName(): string {
@@ -47,19 +47,19 @@ class StudentClass implements StudentClassInterface {
 }
 
 const teacher3: Teacher = {
-  firstName: 'John',
+  firstName: "John",
   fullTimeEmployee: false,
-  lastName: 'Doe',
-  location: 'London',
+  lastName: "Doe",
+  location: "London",
   contract: false,
 };
 
 console.log(teacher3);
 
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
+const director1: Director = {
+  firstName: "John",
+  lastName: "Doe",
+  location: "London",
   fullTimeEmployee: true,
   numberOfReports: 17,
 };
